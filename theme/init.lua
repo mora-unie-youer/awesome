9dd507da70d009a36fe99ef1502c75a7d7a31f38
Mora Unie Youer <mora_unie_youer@riseup.net>
2022-02-09 18:00:45 +0000

    Added theme configuration

---
+++ theme/init.lua
@@ -0,0 +1,38 @@
+-- Standard awesome library
+local gears = require('gears')
+-- Filesystem variables
+local theme_dir = gears.filesystem.get_configuration_dir() .. 'theme/'
+local icon_path = theme_dir .. 'icons/'
+-- Theme table
+local theme = {}
+
+-- Titlebar
+theme.titlebar_size = 20
+-- Titlebar buttons
+theme.titlebar_close_button_normal              = icon_path .. 'close.svg'
+theme.titlebar_close_button_normal_hover        = icon_path .. 'close.svg'
+theme.titlebar_close_button_focus               = icon_path .. 'close.svg'
+theme.titlebar_close_button_focus_hover         = icon_path .. 'close.svg'
+theme.titlebar_minimize_button_normal           = icon_path .. 'minimize.svg'
+theme.titlebar_minimize_button_normal_hover     = icon_path .. 'minimize.svg'
+theme.titlebar_minimize_button_focus            = icon_path .. 'minimize.svg'
+theme.titlebar_minimize_button_focus_hover      = icon_path .. 'minimize.svg'
+theme.titlebar_maximized_button_normal_active   = icon_path .. 'maximized.svg'
+theme.titlebar_maximized_button_normal_inactive = icon_path .. 'maximized.svg'
+theme.titlebar_maximized_button_focus_active    = icon_path .. 'maximized.svg'
+theme.titlebar_maximized_button_focus_inactive  = icon_path .. 'maximized.svg'
+theme.titlebar_ontop_button_normal_active       = icon_path .. 'ontop.svg'
+theme.titlebar_ontop_button_normal_inactive     = icon_path .. 'ontop.svg'
+theme.titlebar_ontop_button_focus_active        = icon_path .. 'ontop.svg'
+theme.titlebar_ontop_button_focus_inactive      = icon_path .. 'ontop.svg'
+theme.titlebar_floating_button_normal_active    = icon_path .. 'floating.svg'
+theme.titlebar_floating_button_normal_inactive  = icon_path .. 'floating.svg'
+theme.titlebar_floating_button_focus_active     = icon_path .. 'floating.svg'
+theme.titlebar_floating_button_focus_inactive   = icon_path .. 'floating.svg'
+theme.titlebar_sticky_button_normal_active      = icon_path .. 'sticky.svg'
+theme.titlebar_sticky_button_normal_inactive    = icon_path .. 'sticky.svg'
+theme.titlebar_sticky_button_focus_active       = icon_path .. 'sticky.svg'
+theme.titlebar_sticky_button_focus_inactive     = icon_path .. 'sticky.svg'
+
+-- Returning theme
+return theme
